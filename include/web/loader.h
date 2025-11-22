708519452ad239dcafa7cc0500949c4c892af6ea
Null-Spectra <onlyonerafi@gmail.com>
2025-11-23 00:16:21 +0600

    Convertd to English

--- include/web/loader.h
+++ include/web/loader.h
@@ -1,7 +1,7 @@
 
 const char loader_html[] = 
 "<!DOCTYPE html>\n"
-"<html lang=\"it\">\n"
+"<html lang=\"en\">\n"
 "<head>\n"
 "    <meta charset=\"UTF-8\">\n"
 "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
